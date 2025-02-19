4dc92aff1011887cca5492dece1a4a41cd4e81d8
Lim Yu De <lim.yude1@gmail.com>
2025-02-19 17:02:26 +0800

    Added JWT token, logout function

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 require('dotenv').config();
 const mysql = require('mysql2');
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt'); //for hashing password
+const jwt = require('jsonwebtoken'); //for managing protected routes
 
 const app = express();
 // Middleware to parse JSON and URL-encoded data
 app.use(express.json()); // For parsing application/json
-app.use(express.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true })); // For parsing form submissions
+
+const blacklistedTokens = new Set(); //Store invalidated token
 
 // MySQL connection
 const db = mysql.createConnection({
@@ -94,13 +97,52 @@ app.post('/api/login', async(req, res) => {
     if (users.length === 0) {
       return res.status(400).json({ error: "Invalid email" });
     }
-    //if valid password
-    if (await bcrypt.compare(password, users[0].password)) {
-      return res.status(200).json({ message: "Login successful", userId: users[0].id });
+    const user = users[0];
+    //if valid password, return token
+    if (await bcrypt.compare(password, user.password)) {
+      const token = jwt.sign( {
+        userId : user.id,
+        name : user.username,
+        role : user.role,
+      }, process.env.JWT_SECRET);
+      return res.status(200).json({ message: "Login successful", user: user.id, token : token });
     } else { //invalid password
       return res.status(400).json({ message: "Incorrect password" });
     }
 });
+
+//Logout
+app.post('/api/logout', authenticateToken, (req, res) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1]; // Extract token
+
+  if (!token) {
+    return res.status(401).json({ error: 'No token provided' });
+  }
+  blacklistedTokens.add(token); //Invalidate token
+  res.status(200).json({ message: 'Logout successful' });
+});
+
+//Middleware to authenticate JWT token
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null) { //No token is sent
+    return res.status(401).json({ error : "Unauthorized access! Please login"});
+  }
+  if (blacklistedTokens.has(token)) {//Token is invalidated 
+    return res.status(403).json({ error : "Session invalid, please login again"});
+
+  }
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {//token is invalid
+      return res.status(403).json({ error : "Invalid or expired session"});
+
+    }
+    req.user = user;//set user
+    next();
+  });
+}
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
