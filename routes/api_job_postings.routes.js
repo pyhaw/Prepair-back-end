2ac7e27069146c081ba736ce2653c34c2a39318b
zackeus <zchoo2001@gmail.com>
2025-03-25 01:30:14 +0800

    Users can now view their active jobs/bids and fixers can bid for jobs

--- routes/api_job_postings.routes.js
+++ routes/api_job_postings.routes.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const { createJobPosting, fetchJobPosting, fetchJobPostingByUserId } = require("../handlers/api_job_posting.handlers");
+const {
+  createJobPosting,
+  fetchJobPosting,
+  fetchJobPostingByUserId,
+  createJobBid,
+  fetchJobBids,
+  fetchActiveBidsForFixer,
+  updateJobPosting,
+  updateJobBid,
+} = require("../handlers/api_job_posting.handlers");
 const { authenticateToken } = require("../middleware/auth");
 
 // Ensure Express Router is properly initialized
@@ -15,6 +24,13 @@ router.get("/job-postings", authenticateToken, fetchJobPosting);
 
 // Route to fetch job postings by user ID
 router.get("/job-postings/:userId", fetchJobPostingByUserId);
+router.post("/job-bids", authenticateToken, createJobBid);
+
+router.get("/job/:id/bids", authenticateToken, fetchJobBids);
+
+router.get("/job-bids", authenticateToken, fetchActiveBidsForFixer);
+router.put("/job-bids/:id", authenticateToken, updateJobBid);
+router.put("/job-postings/:id", authenticateToken, updateJobPosting);
 
 // Export the router correctly
 module.exports = router;
--- routes/api_users.routes.js
+++ routes/api_users.routes.js
@@ -23,12 +23,16 @@ const { uploadSingle } = require("../middleware/upload"); // Import the middlewa
 router.post("/register", registerUser); // Register a new user
 router.post("/login", loginUser); // Login a user
 router.get("/userProfile/:userId", authenticateToken, getUserProfile); // Get user profile
-router.put("/userProfile/:userId", authenticateToken, uploadSingle, updateUserProfileImpl);
+router.put(
+  "/userProfile/:userId",
+  authenticateToken,
+  uploadSingle,
+  updateUserProfileImpl
+);
 router.delete("/users/:userId", authenticateToken, deleteUserHandler); // Delete a user
 router.get("/users", getAllUsersHandler); // Get all users (Admin)
 router.post("/forgot-password", sendOTPToEmail);
 router.post("/verifyOtp", verifyOtp);
-router.post("/reset-password", resetPassword)
-
+router.post("/reset-password", resetPassword);
 
 module.exports = router;
